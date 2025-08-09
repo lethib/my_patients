5b9d07f413929f687084060686f193bf7eb9b55a
Thibs <88190704+lethib@users.noreply.github.com>
2025-08-09 10:51:21 +0200

    refactor: remove unused (for now) user fields (#1)

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -3,6 +3,7 @@
 pub use sea_orm_migration::prelude::*;
 mod m20220101_000001_users;
 
+mod m20250809_080459_remove_unnecessary_cols_from_users;
 pub struct Migrator;
 
 #[async_trait::async_trait]
@@ -10,6 +11,7 @@ impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
         vec![
             Box::new(m20220101_000001_users::Migration),
+            Box::new(m20250809_080459_remove_unnecessary_cols_from_users::Migration),
             // inject-above (do not remove this comment)
         ]
     }
---
+++ migration/src/m20250809_080459_remove_unnecessary_cols_from_users.rs
@@ -0,0 +1,56 @@
+use loco_rs::schema::*;
+use sea_orm_migration::prelude::*;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, m: &SchemaManager) -> Result<(), DbErr> {
+        remove_column(m, "users", "api_key").await?;
+        remove_column(m, "users", "reset_token").await?;
+        remove_column(m, "users", "reset_sent_at").await?;
+        remove_column(m, "users", "email_verification_token").await?;
+        remove_column(m, "users", "email_verification_sent_at").await?;
+        remove_column(m, "users", "email_verified_at").await?;
+        remove_column(m, "users", "magic_link_token").await?;
+        remove_column(m, "users", "magic_link_expiration").await?;
+        Ok(())
+    }
+
+    async fn down(&self, m: &SchemaManager) -> Result<(), DbErr> {
+        add_column(m, "users", "api_key", ColType::StringUniq).await?;
+        add_column(m, "users", "reset_token", ColType::StringNull).await?;
+        add_column(
+            m,
+            "users",
+            "reset_sent_at",
+            ColType::TimestampWithTimeZoneNull,
+        )
+        .await?;
+        add_column(m, "users", "email_verification_token", ColType::StringNull).await?;
+        add_column(
+            m,
+            "users",
+            "email_verification_sent_at",
+            ColType::TimestampWithTimeZoneNull,
+        )
+        .await?;
+        add_column(
+            m,
+            "users",
+            "email_verified_at",
+            ColType::TimestampWithTimeZoneNull,
+        )
+        .await?;
+        add_column(m, "users", "magic_link_token", ColType::StringNull).await?;
+        add_column(
+            m,
+            "users",
+            "magic_link_expiration",
+            ColType::TimestampWithTimeZoneNull,
+        )
+        .await?;
+        Ok(())
+    }
+}
