06a744dccb36379b18946cfc5b44d08445d7898c
Thibs <88190704+lethib@users.noreply.github.com>
2025-09-13 17:58:14 +0200

    feat(auth): logout on 401 (#36)

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -4,6 +4,7 @@ import axios, {
   type AxiosInstance,
   type AxiosRequestConfig,
 } from "axios";
+import { logout } from "@/lib/authUtils";
 import { APIHooks } from "./hooks";
 
 export type APIError = {
@@ -33,6 +34,10 @@ class MyPatientsAPI {
     this.client.interceptors.response.use(
       (response) => response,
       (error: AxiosError<APIError>) => {
+        if (error.response?.status === 401) {
+          logout();
+        }
+
         throw error;
       },
     );
