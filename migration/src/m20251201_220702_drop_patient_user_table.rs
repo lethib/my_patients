1530a34724bec2b74f643f6cb0d4f9bb9796626a
Thibs <88190704+lethib@users.noreply.github.com>
2025-12-01 23:19:23 +0100

    chore: drop patient user table (#119)

---
+++ migration/src/m20251201_220702_drop_patient_user_table.rs
@@ -0,0 +1,23 @@
+use sea_orm_migration::prelude::*;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+  async fn up(&self, m: &SchemaManager) -> Result<(), DbErr> {
+    m.drop_table(Table::drop().table(PatientUsers::Table).to_owned())
+      .await
+  }
+
+  async fn down(&self, _m: &SchemaManager) -> Result<(), DbErr> {
+    Err(DbErr::Migration(
+      "Cannot rollback: patient_users table removal is irreversible".to_string(),
+    ))
+  }
+}
+
+#[derive(DeriveIden)]
+enum PatientUsers {
+  Table,
+}
